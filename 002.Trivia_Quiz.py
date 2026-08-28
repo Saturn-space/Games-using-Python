b8479578769aab7f813e8d82dd9206b4dc05d1ac
Mohan Prasad <unknonfiction@gmail.com>
2026-08-28 11:04:29 +0530

    Refactor trivia quiz into start function
    Refactored trivia quiz logic into a function for better structure.

--- 002.Trivia_Quiz.py
+++ 002.Trivia_Quiz.py
@@ -3,17 +3,22 @@ quiz = {
 "Name the Largest Mammal on Earth ? " : "blue whale",
 "What are the Four Magical Letters ? " : "love" 
 }
-score = 0
-Q = 1
-print("Your Trivia Quiz Starts Now !\n")
-for i in quiz:
-    print(f"Q{Q}. {i}")
-    ans = (input("Your Answers: ").lower()).strip()
-    if(ans==quiz[i]):
-        print("Correct ✅\n")
-        score +=1;
-    else:
-        print(f"❌ Wrong the correct answer was {quiz[i]} \n")
-    Q+=1;
 
-print(f"🏆 Your Final Score is {score}/{len(quiz)} 🌟")
+def start(trivia):
+    score = 0
+    total = 3
+    Q = 1
+    print("Your Trivia Quiz Starts Now !\n")
+    for i in trivia:
+        print(f"Q{Q}. {i}")
+        ans = (input("Your Answers: ").lower()).strip()
+        if(ans==trivia[i]):
+            print("Correct ✅\n")
+            score +=1;
+        else:
+            print(f"❌ Wrong the correct answer was {trivia[i]} \n")
+        Q+=1;
+
+    print(f"🏆 Your Final Score is {score}/{total} 🌟")
+
+start(quiz)
